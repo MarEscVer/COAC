dd708085edfc3513ec7e88b847acab0585219fbb
María Escribano Verde <maria.escribanoverde@gmail.com>
2023-03-06 19:26:27 +0100

    Documentación de las Clases Listado y Ordenaciones

--- COAC/src/view/Listado.java
+++ COAC/src/view/Listado.java
@@ -3,8 +3,17 @@ package view;
 import controller.COAC;
 import model.Agrupacion;
 
+/**
+ * <h2>Clase Listado, creada para gestionar el listado de Objetos para la vista</h2>
+ * @author maria
+ * @since 06-03-2022
+ */
 public class Listado {
 	
+	/**
+	 * Mostrar por pantalla un listado de todas las Agrupaciones incluidas en un Array que no sean nulas
+	 * @param a <i>Array de Agrupaciones</i>
+	 */
 	public static void listar(Agrupacion[] a) {
 		int cont = 1;
 		for (int i = 0; i < a.length; i++) {
@@ -14,36 +23,57 @@ public class Listado {
 		}
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todas las Agrupaciones de un COAC
+	 */
 	static void listarTodo() {	
 		Agrupacion [] aux = Principal.coac.getAgrupaciones();
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todas las Chirigotas de un COAC
+	 */
 	public static void listarChirigotas() {
 		Agrupacion [] aux = COAC.obtenerChirigota(Principal.coac.getAgrupaciones());
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todos los Coros de un COAC
+	 */
 	public static void listarCoros() {	
 		Agrupacion [] aux = COAC.obtenerCoro(Principal.coac.getAgrupaciones());
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todos los Cuartetos de un COAC
+	 */
 	public static void listarCuartetos() {	
 		Agrupacion [] aux = COAC.obtenerCuarteto(Principal.coac.getAgrupaciones());
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todas las Comparsas de un COAC
+	 */
 	public static void listarComparsas() {	
 		Agrupacion [] aux = COAC.obtenerComparsa(Principal.coac.getAgrupaciones());
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todos los Riomanceros de un COAC
+	 */
 	public static void listarRomanceros() {
 		Agrupacion [] aux = COAC.obtenerRomancero(Principal.coac.getAgrupaciones());
 		listar(aux);
 	}
 	
+	/**
+	 * Mostrar por pantalla un listado de todas las Agrupaciones Oficiales de un COAC
+	 */
 	public static void listarOficiales() {
 		Agrupacion [] aux = COAC.obtenerOficiales(Principal.coac.getAgrupaciones());
 		listar(aux);
--- COAC/src/view/Ordenaciones.java
+++ COAC/src/view/Ordenaciones.java
@@ -1,18 +1,34 @@
 package view;
 
+/**
+ * <h2>Clase Ordenaciones, creada para gestionar la ordenación de Objetos en la vista</h2>
+ * @author maria
+ * @since 06-03-2022
+ */
 public class Ordenaciones {
 	
-	// Error elementos nulos
+	/**
+	 * Ordenar por nombre el objeto COAC y listar todo por pantalla
+	 * @see Listado
+	 */
 	public static void ordenarNombre() {
 		Principal.coac.ordenar_por_nombre();
 		Listado.listarTodo();
 	}
 	
+	/**
+	 * Ordenar por nombre del autor el objeto COAC y listar todo por pantalla
+	 * @see Listado
+	 */
 	public static void ordenarAutor() {
 		Principal.coac.ordenar_por_autor();;
 		Listado.listarTodo();
 	}
 	
+	/**
+	 * Ordenar por nombre el nombre del auutor de la música y el de la letra el objeto COAC y listar todo por pantalla
+	 * @see Listado
+	 */
 	public static void ordenarMusicaLetra() {
 		Principal.coac.ordenar_por_autor_musica_letra();
 		Listado.listarTodo();
